316bba744cca064abb5bdeb4c3daa1abe7daf2c6
Anthony Le <quangld@gearvn.com>
2021-08-13 19:35:27 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import * as express from 'express';
 import { AppModule } from './app.module';
 import { join } from 'path';
-const fs = require('fs');
+// const fs = require('fs');
 
-const httpsOptions = {
-  key: fs.readFileSync('./secrets/private-key.pem'),
-  cert: fs.readFileSync('./secrets/public-certificate.pem'),
-};
+// const httpsOptions = {
+//   key: fs.readFileSync('./secrets/private-key.pem'),
+//   cert: fs.readFileSync('./secrets/public-certificate.pem'),
+// };
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions,
-  });
+  const app = await NestFactory.create(AppModule);
   app.use(express.static(join(process.cwd(), '../public/')));
-  await app.listen(8088);
+  await app.listen(8088, ()=> console.log('server started'));
 }
 bootstrap();
