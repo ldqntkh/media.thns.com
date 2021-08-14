d92643943b42e550a5f504c573951f38fceb0e83
Anthony Le <quangld@gearvn.com>
2021-08-14 18:19:11 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5502', '*'],
+    origin: ['http://localhost:5502', 'https://tinhocngoisao.com'],
     methods: 'GET, PUT, POST, DELETE',
     allowedHeaders: 'Content-Type, Authorization',
 });
